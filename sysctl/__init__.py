212e4202460be54394bc133b1f55983cd2b37f16
Stefan Grönke <stefan@gronke.net>
2019-02-14 01:59:46 +0000

    read sysctl values

--- sysctl/__init__.py
+++ sysctl/__init__.py
@@ -11,6 +11,154 @@ CTL_MAXNAME = ctypes.c_uint(24)
 T_OID = (ctypes.c_int * 2)
 
 
+class CtlType:
+    min_size = 0
+    data: bytes
+    ctype: typing.Optional[type] = None
+    unpack_format: typing.Optional[str] = None
+
+    def __init__(self, data: bytes) -> None:
+        self.data = data
+
+    @property
+    def value(self) -> typing.Any:
+        if self.unpack_format is None:
+            return self.data.value
+        value, = struct.unpack(f"<{self.unpack_format}", self.data)
+        return value
+
+    def __str__(self) -> str:
+        value = self.value
+        if isinstance(value, bytes) is True:
+            return value.decode()
+        return str(value)
+
+
+class NodeType(CtlType):
+    pass
+
+
+class IntType(CtlType):
+    ctype = ctypes.c_int
+    min_size = ctypes.sizeof(ctypes.c_int)
+    unpack_format = "i"
+
+
+class StringType(CtlType):
+    pass
+
+
+class Int64Type(CtlType):
+    ctype = ctypes.c_int64
+    min_size = ctypes.sizeof(ctypes.c_int64)
+    unpack_format = "q"
+
+
+class StructType(CtlType):
+    pass
+
+
+class OpaqueType(CtlType):
+    pass
+
+
+class UIntType(CtlType):
+    ctype = ctypes.c_uint
+    min_size = ctypes.sizeof(ctypes.c_uint)
+    unpack_format = "I"
+
+
+class LongType(CtlType):
+    ctype = ctypes.c_long
+    min_size = ctypes.sizeof(ctypes.c_long)
+    unpack_format = "q"
+
+
+class ULongType(CtlType):
+    ctype = ctypes.c_ulong
+    min_size = ctypes.sizeof(ctypes.c_ulong)
+    unpack_format = "Q"
+
+
+class UInt64Type(CtlType):
+    ctype = ctypes.c_uint64
+    min_size = ctypes.sizeof(ctypes.c_uint64)
+    unpack_format = "Q"
+
+
+class UInt8Type(CtlType):
+    ctype = ctypes.c_uint8
+    min_size = ctypes.sizeof(ctypes.c_uint8)
+    unpack_format = "B"
+
+
+class UInt16Type(CtlType):
+    ctype = ctypes.c_uint16
+    min_size = ctypes.sizeof(ctypes.c_uint16)
+    unpack_format = "H"
+
+
+class Int8Type(CtlType):
+    ctype = ctypes.c_int8
+    min_size = ctypes.sizeof(ctypes.c_int8)
+    unpack_format = "b"
+
+
+class Int16Type(CtlType):
+    ctype = ctypes.c_int16
+    min_size = ctypes.sizeof(ctypes.c_int16)
+    unpack_format = "h"
+
+
+class Int32Type(CtlType):
+    ctype = ctypes.c_int32
+    min_size = ctypes.sizeof(ctypes.c_int32)
+    unpack_format = "i"
+
+
+class UInt32Type(CtlType):
+    ctype = ctypes.c_uint32
+    min_size = ctypes.sizeof(ctypes.c_uint32)
+    unpack_format = "I"
+
+
+def identify_type(kind: int, fmt: bytes) -> CtlType:
+    ctl_type = kind & 0xF
+    if ctl_type == 1:
+        return NodeType
+    elif ctl_type == 2:
+        return IntType
+    elif ctl_type == 3:
+        return StringType
+    elif ctl_type == 4:
+        return Int64Type
+    elif ctl_type == 5:
+        # return StructType if (fmt[0:1] == b"S") else OpaqueType
+        return OpaqueType
+    elif ctl_type == 6:
+        return UIntType
+    elif ctl_type == 7:
+        return LongType
+    elif ctl_type == 8:
+        return ULongType
+    elif ctl_type == 9:
+        return UInt64Type
+    elif ctl_type == 10:
+        return UInt8Type
+    elif ctl_type == 11:
+        return UInt16Type
+    elif ctl_type == 12:
+        return Int16Type
+    elif ctl_type == 13:
+        return Int16Type
+    elif ctl_type == 14:
+        return Int32Type
+    elif ctl_type == 15:
+        return UInt32Type
+    else:
+        raise Exception(f"Invalid ctl_type: {ctl_type}")
+
+
 class Sysctl:
 
     _name: typing.Optional[str]
@@ -18,6 +166,7 @@ class Sysctl:
     _kind: typing.Optional[int]
     _fmt = typing.Optional[bytes]
     _size: typing.Optional[int]
+    _value: typing.Optional[str]
 
     def __init__(
         self,
@@ -29,6 +178,7 @@ class Sysctl:
         self._kind = None
         self._fmt = None
         self._size = None
+        self._value = None
 
     @property
     def oid(self) -> typing.List[int]:
@@ -63,6 +213,12 @@ class Sysctl:
         if self._size is None:
             self._size = self.oidsize(self.oid, self.ctl_type)
         return self._size
+
+    @property
+    def value(self) -> int:
+        if self._value is None:
+            self._value = self.oidvalue(self.oid, self.size, self.ctl_type)
+        return self._value
 
     def __query_kind_and_fmt(self) -> None:
         self._kind, self._fmt = self.oidfmt(self.oid)
@@ -124,7 +280,7 @@ class Sysctl:
         return (kind, fmt)
 
     @staticmethod
-    def oidsize(oid: typing.List[int], ctl_type: str) -> bytes:
+    def oidsize(oid: typing.List[int], ctl_type: CtlType) -> bytes:
 
         oid_type = ctypes.c_int * len(oid)
         _oid = (oid_type)(*oid)
@@ -141,58 +297,47 @@ class Sysctl:
             0
         )
 
-        min_sizes = {
-            "node": 0,
-            "int": ctypes.sizeof(ctypes.c_int),
-            "string": 0,
-            "s64": ctypes.sizeof(ctypes.c_int64),
-            "struct": 0,
-            "opaque": 0,
-            "uint": ctypes.sizeof(ctypes.c_uint),
-            "long": ctypes.sizeof(ctypes.c_long),
-            "ulong": ctypes.sizeof(ctypes.c_ulong),
-            "u64": ctypes.sizeof(ctypes.c_uint8),
-            "u8": ctypes.sizeof(ctypes.c_uint8),
-            "u16": ctypes.sizeof(ctypes.c_uint16),
-            "s8": ctypes.sizeof(ctypes.c_int8),
-            "s16": ctypes.sizeof(ctypes.c_int16),
-            "s32": ctypes.sizeof(ctypes.c_int32),
-            "u32": ctypes.sizeof(ctypes.c_uint32)
-        }
+        return max(length.value, ctl_type.min_size)
 
-        return max(length.value, min_sizes[ctl_type])
+    @staticmethod
+    def oidvalue(
+        oid: typing.List[int],
+        size: int,
+        ctl_type: CtlType
+    ) -> bytes:
+
+        # ToDo: check if value is readable
+
+        oid_type = ctypes.c_int * len(oid)
+        _oid = (oid_type)(*oid)
+        p_oid = ctypes.POINTER(oid_type)(_oid)
+
+        buf_type = ctypes.c_char * size
+        buf = buf_type()
+        p_buf = ctypes.POINTER(buf_type)(buf)
+        p_buf_void = ctypes.cast(p_buf, ctypes.c_void_p)
+
+        buf_length = ctypes.sizeof(buf)
+        p_buf_length = ctypes.POINTER(ctypes.c_int)(ctypes.c_int(buf_length))
+
+        sysctl.libc.dll.sysctl(
+            p_oid,
+            ctypes.c_uint32(len(oid)),
+            p_buf_void,
+            p_buf_length,
+            None,
+            0
+        )
+
+        return ctl_type(buf)
 
     @property
-    def ctl_type(self) -> str:
+    def ctl_type(self) -> CtlType:
         return self.get_ctl_type(self.kind, self.fmt)
 
     @staticmethod
-    def get_ctl_type(kind: int, fmt: bytes) -> str:
-        ctl_types = [
-            "node",
-            "int",
-            "string",
-            "s64",
-            ("struct", "opaque",),
-            "uint",
-            "long",
-            "ulong",
-            "u64",
-            "u8",
-            "u16",
-            "s8",
-            "s16",
-            "s32",
-            "u32"
-        ]
-        ctl_type = kind & 0xF
-        if ctl_type == 0:
-            raise Exception("Invalid ctl_type")
-
-        if ctl_type == 5:
-            return "struct" if (fmt[0:1] == b"S") else "opaque"
-        else:
-            return ctl_types[ctl_type-1]
+    def get_ctl_type(kind: int, fmt: bytes) -> CtlType:
+        return identify_type(kind, fmt)
 
 
 # Sysctl("security.jail.enforce_statfs")
@@ -200,4 +345,3 @@ class Sysctl:
 # Sysctl("security.jail.param.path")
 # Sysctl("security.jail.param.osrelease")
 # Sysctl("security.jail.param.children.max")
-
