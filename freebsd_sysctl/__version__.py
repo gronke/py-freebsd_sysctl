62071b7478d014fd96a896ad8dfd8a8d848afc0f
Stefan Grönke <stefan@gronke.net>
2020-04-26 18:10:56 +0200

    read setup.py version from VERSION file or use git sha

--- freebsd_sysctl/__version__.py
+++ freebsd_sysctl/__version__.py
@@ -1,3 +1,18 @@
-VERSION = (0, 0, 8)
+def read_first_line(relative_filename: str) -> str:
+    absolute_path = os.path.join(os.getcwd(), relative_filename)
+    with open(absolute_path, "r", encoding="UTF-8") as f:
+        return f.readline().strip()
 
-__version__ = '.'.join(map(str, VERSION))
+
+try:
+    VERSION = read_first_line("VERSION")
+except:
+    # last git commit SHA is the version
+    line = read_first_line(".git/HEAD")
+    if line.startswith("ref: ") is False:
+        VERSION = line
+    else:
+        ref = line[5:]
+        VERSION = read_first_line(f".git/{ref}")
+
+__version__ = VERSION
