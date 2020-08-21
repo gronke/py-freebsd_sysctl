bf46fb1beef97b0406af45778d45760949f7fe1d
Mina Galić <me+git@igalic.co>
2020-08-21 21:53:40 +0200

    move metadata into setup.cfg
    the only "complication" is that we have to expose __version__ at module
    level (because i couldn't figure out a better way to do it)
    
    but now, our entire setup.py is a single `setup()` call

--- setup.py
+++ setup.py
@@ -1,36 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
-import os.path
-import sys
-from setuptools import find_packages, setup
+from setuptools import setup
 
-cwd = os.getcwd()
-
-about = {}
-version_file = os.path.join(os.getcwd(), "freebsd_sysctl", '__version__.py')
-with open(version_file, encoding="utf-8") as f:
-    VERSION = exec(f.read(), about)
-
-with open(os.path.join(cwd, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-
-setup(
-	name="freebsd-sysctl",
-	version=about['__version__'],
-	description="Native Python wrapper for FreeBSD sysctls using libc.",
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	url="https://github.com/gronke/py-freebsd_sysctl",
-	author="Stefan Grönke",
-	author_email="stefan@gronke.net",
-	python_requires=">=3.6",
-	platforms=['FreeBSD'],
-	license="BSD 2-Clause License",
-	classifiers=[
-		"License :: OSI Approved :: BSD 2-Clause License"
-	],
-	tests_require=["pytest-runner", "pytest"],
-	packages=find_packages(exclude=('tests',))
-)
+setup()
